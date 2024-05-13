98725cb63c2e9c10ffd804346a6a1dd4c8c80e27
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-05-14 04:32:36 +0900

    entrypointの追加

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# RAILS_ENV=production bundle exec rake assets:clobber
+# RAILS_ENV=production bundle exec rake assets:precompile
+# RAILS_ENV=production bundle exec rake assets:clean
+# RAILS_ENV=production bundle exec rake db:migrate
+
+exec "$@"
