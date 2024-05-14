3c1c798a92a9efb2c7317d173fe2a3d82e4bb59c
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-05-14 15:21:21 +0900

    itemコントローラーとルーティングの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :item_tags, dependent: :destroy
+  has_many :tags, through: :item_tags
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :item_url, presence: true, length: { maximum: 255 }
+  # 説明欄は必須ではない
+  validates :description, length: { maximum: 255 }, allow_blank: true
+
 end
